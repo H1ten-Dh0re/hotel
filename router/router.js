f1765d4861b756dba0f47ebaab19115b94f382e9
Aditya <aditya.chaudhary1558@gmail.com>
2024-04-21 21:25:24 +0530

    feat: added the logic for signIn/signUp page

---
+++ router/router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const user = require('../models/user');
+const router = express.Router();
+
+
+router.get('/', userController.home);
+
+router.get('/about', userController.about);
+
+router.post('/signIn', userController.signIn);
+
+router.post('/signUp', userController.signUp);
+
+module.exports = router;
